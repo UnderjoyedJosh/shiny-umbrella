a10d210c288021f2eb8f2163c9b64d45c21db027
Joshua Corbett <j.corbett998@gmail.com>
2022-08-25 02:25:10 +0100

    Update flasktests.py

--- flasktests.py
+++ flasktests.py
@@ -4,7 +4,7 @@ class SimpleTest(unittest.TestCase):
   
     # Returns True or False. 
     def test(self):        
-        self.assertTrue(False)
+        self.assertTrue(True)
   
 if __name__ == '__main__':
     unittest.main()
